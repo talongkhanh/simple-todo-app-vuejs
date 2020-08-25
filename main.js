54763c3143a128f133e6a0c760e446eb8cf4b6da
Ta Long Khanh <longkhanh02092000@gmail.com>
2020-08-25 22:09:33 +0700

    Delete Todo

---
+++ main.js
@@ -0,0 +1,18 @@
+new Vue({
+	el: '#app',
+	data: {
+		todos: [
+		{ id: 1, text: 'Nấu Cơm', done: false },
+		{ id: 2, text: 'Rửa Bát', done: true },
+		{ id: 3, text: 'Đánh Liên Minh', done: false },
+		]
+	},
+	methods: {
+		toggle(todo) {
+			todo.done = !todo.done;
+		},
+		deleteTodo(id) {
+			this.todos = this.todos.filter((todo) => todo.id != id)
+		}
+	},
+});
