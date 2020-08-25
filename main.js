7d370efa2435fe46e6eed971e214dcb63a35dd0f
Ta Long Khanh <longkhanh02092000@gmail.com>
2020-08-25 22:56:18 +0700

    local storage

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 const app = new Vue({
 	el: '#app',
 	data: {
-		todos: [
-		{ id: 1, text: 'Nấu Cơm', done: false },
-		{ id: 2, text: 'Rửa Bát', done: true },
-		{ id: 3, text: 'Đánh Liên Minh', done: false },
-		]
+		todos: JSON.parse(localStorage.getItem('todos'))
 	},
 	methods: {
 		toggle(todo) {
 			todo.done = !todo.done;
+			localStorage.setItem('todos', JSON.stringify(this.todos));
 		},
 		deleteTodo(id) {
 			this.todos = this.todos.filter((todo) => todo.id != id);
-			console.log(JSON.parse(JSON.stringify(this.todos)));
+			// console.log(JSON.parse(JSON.stringify(this.todos)));
+			localStorage.setItem('todos', JSON.stringify(this.todos));
 		},
 		addTodo(e) {
+			if(!this.todos) {
+				this.todos = [];
+			}
 			let id = Math.ceil(Math.random() * 99999999);
 			let text = e.target.value;
 			let done = false;
 			let newTodo = { id, text, done };
 			this.todos.push(newTodo);
 			e.target.value = '';
-			console.log(JSON.parse(JSON.stringify(this.todos)));
+			// console.log(JSON.parse(JSON.stringify(this.todos)));
+			localStorage.setItem('todos', JSON.stringify(this.todos));
 		},
 		editTodo(e) {
 			let newText = e.target.innerText;
@@ -34,7 +36,8 @@ const app = new Vue({
 				}
 			}
 
-			console.log(JSON.parse(JSON.stringify(this.todos)));
+			// console.log(JSON.parse(JSON.stringify(this.todos)));
+			localStorage.setItem('todos', JSON.stringify(this.todos));
 		}
 	},
 });
